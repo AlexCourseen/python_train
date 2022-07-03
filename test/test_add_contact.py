4612ffbbabbecf4cf50909775140c5528b0c15e8
sandw <beardkid7@gmail.com>
2022-07-03 11:05:15 +0300

    login, logout in fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,8 +3,7 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.add(Contact(firstName="Ivan", lastName="Testov"))
-    app.session.logout()
+
 
 
