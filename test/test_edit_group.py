0df834fa351bf9949326a580ed718f9b0b924abc
sandw <beardkid7@gmail.com>
2022-07-03 10:34:48 +0300

    edit methods create,edit group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 
-def test_edit_first_group(app):
+def test_edit_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="EditName", header="EditHeader", footer="EditFooter"))
+    app.group.edit_group(Group(name="EditName"))
+    app.session.logout()
+
+
+def test_edit_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="EditHeader"))
     app.session.logout()
