b7c9dbe21fd989f7347d7f76e4ae7f97ed827da6
sandw <beardkid7@gmail.com>
2022-07-13 23:53:44 +0300

    ex#11 asserts and eq in contact and groups

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,7 +2,14 @@ from model.contact import Contact
 
 
 def test_delete_first_contact(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstName='testIvan')
     if app.contact.count_contacts() == 0:
-        app.contact.add(Contact(firstName='testIvan'))
+        app.contact.add(contact)
     app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    """удаление первого элемента, т.к. индекс 0-включается,а 1-не включается"""
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
 
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,14 +1,31 @@
 from model.contact import Contact
 
 
-def test_edit_contact_(app):
+def test_edit_contact_firstName(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstName='Edit')
+    contact.id = old_contacts[0].id
+    contact.lastName = old_contacts[0].lastName
     if app.contact.count_contacts() == 0:
         app.contact.add(Contact(firstName='testIvan'))
-    app.contact.edit_first_contact(Contact(firstName="Edit"))
+    app.contact.edit_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_edit_first_contact(app):
+def test_edit_contact_lastName(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(lastName="Editovich")
+    contact.id = old_contacts[0].id
+    contact.firstName = old_contacts[0].firstName
     if app.contact.count_contacts() == 0:
-        app.contact.add(Contact(lastName='testIvanovich'))
-    app.contact.edit_first_contact(Contact(lastName="Editovich"))
+        app.contact.add(lastName='testIvanovich')
+    app.contact.edit_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
 
